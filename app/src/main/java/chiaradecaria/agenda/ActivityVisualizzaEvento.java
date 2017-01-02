a8a6caa898e97a42ed94ba8006c5d3ad90b02831
marc98kr <decariamarco98@gmail.com>
2017-01-02 23:35:40 +0100

    Aggiunte varie
    - Aggiunta la distanza e il tempo dal luogo dell'evento
    - varie

--- app/src/main/java/chiaradecaria/agenda/ActivityVisualizzaEvento.java
+++ app/src/main/java/chiaradecaria/agenda/ActivityVisualizzaEvento.java
@@ -1,20 +1,50 @@
 package chiaradecaria.agenda;
 
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
-public class ActivityVisualizzaEvento extends AppCompatActivity {
+public class ActivityVisualizzaEvento extends AppCompatActivity implements LocationListener{
     DBManager db;
     long idEvento;
+    double lat, lng;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizza_evento);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 1, this);
+        }catch(SecurityException ex){
+            Log.e("A.VisualizzaEventi", "Errore onCreate(): " + ex.getLocalizedMessage());
+            Toast.makeText(this, "Impossibile ottenere la posizione", Toast.LENGTH_SHORT);
+        }
+
         idEvento = getIntent().getExtras().getLong("id_evento");
         Log.i("A.VisualizzaEvento", "id evento: " + idEvento);
         db = new DBManager(this);
@@ -22,18 +52,23 @@ public class ActivityVisualizzaEvento extends AppCompatActivity {
     }
     /**Metodo che prende l'evento dal db e lo mostra*/
     private void mostraEvento(){
-        Cursor cursor = db.getEvento(idEvento);
-        Log.i("A.VisualizzaEvento", "" + cursor.getColumnIndex(DBStrings.TITOLO_EVENTO));
-        String titolo = cursor.getString(1/*cursor.getColumnIndex(DBStrings.TITOLO_EVENTO)*/);
-        String luogo = cursor.getString(cursor.getColumnIndex(DBStrings.LUOGO));
-        String data = cursor.getString(cursor.getColumnIndex(DBStrings.DATA));
-        String oraInizio = cursor.getString(cursor.getColumnIndex(DBStrings.ORA_INIZIO));
-        String oraFine = cursor.getString(cursor.getColumnIndex(DBStrings.ORA_FINE));
-        ((EditText) findViewById(R.id.txtTitolo)).setText(titolo);
-        ((EditText) findViewById(R.id.txtLuogo)).setText(luogo);
-        ((EditText) findViewById(R.id.txtData)).setText(data);
-        ((EditText) findViewById(R.id.txtOraInizio)).setText(oraInizio);
-        ((EditText) findViewById(R.id.txtOraFine)).setText(oraFine);
+        try {
+            Cursor cursor = db.getEvento(idEvento);
+            cursor.moveToFirst();
+            Log.i("A.VisualizzaEvento", "" + cursor.getColumnIndex(DBStrings.TITOLO_EVENTO));
+            String titolo = cursor.getString(cursor.getColumnIndex(DBStrings.TITOLO_EVENTO));
+            String luogo = cursor.getString(cursor.getColumnIndex(DBStrings.LUOGO));
+            String data = cursor.getString(cursor.getColumnIndex(DBStrings.DATA));
+            String oraInizio = cursor.getString(cursor.getColumnIndex(DBStrings.ORA_INIZIO));
+            String oraFine = cursor.getString(cursor.getColumnIndex(DBStrings.ORA_FINE));
+            ((EditText) findViewById(R.id.txtTitolo)).setText(titolo);
+            ((EditText) findViewById(R.id.txtLuogo)).setText(luogo);
+            ((EditText) findViewById(R.id.txtData)).setText(data);
+            ((EditText) findViewById(R.id.txtOraInizio)).setText(oraInizio);
+            ((EditText) findViewById(R.id.txtOraFine)).setText(oraFine);
+        }catch(CursorIndexOutOfBoundsException ex){
+            Log.e("A.VisualizzaEvento", ex.getLocalizedMessage());
+        }
     }
     /**Metodo che aggiorna l'evento*/
     private void aggiornaEvento(){
@@ -44,9 +79,127 @@ public class ActivityVisualizzaEvento extends AppCompatActivity {
         String oraFine = ((EditText) findViewById(R.id.txtOraFine)).getText().toString();
         db.aggiornaEvento(idEvento, titolo, luogo, data, oraInizio, oraFine);
     }
+    private String[] estraiDati(String risposta){
+        try{
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            InputSource source = new InputSource();
+            source.setCharacterStream(new StringReader(risposta));
+            Document document = documentBuilder.parse(source);
+            String durata = document.getElementsByTagName("duration").item(0).getTextContent();
+            String distanza = document.getElementsByTagName("distance").item(0).getTextContent();
+            durata = estraiDurata(durata);
+            distanza = estraiDistanza(distanza);
+            Log.i("A.VisualizzaEventi", "Dati: " + distanza + " (" + durata + ")");
+            return new String[]{distanza, durata};
+        }catch(Exception e){
+            Log.e("A.VisualizzaEvento", "Errore estraiDati(String): " + e.getLocalizedMessage());
+            return null;
+        }
+    }
+    private String estraiDistanza(String distanza){
+        distanza = distanza.substring(1, distanza.indexOf("\n", 1)).replaceAll("\\s", "");
+        double d = Double.parseDouble(distanza);
+        d /= 1000;
+        return d + " km";
+    }
+    private String estraiDurata(String durata){
+        durata = durata.substring(1, durata.indexOf("\n", 1)).replaceAll("\\s", "");
+        long durataTot = Long.parseLong(durata);
+        //La durata inviata dal server di Google Maps è espressa in secondi
+        long ore = durataTot / 3600;
+        long minuti = (durataTot % 3600) / 60;
+        return "" + ore + ":" + minuti;
+    }
+    private String richiediDistanza(String urlRichiesta, String urlParameters){
+        //Creo un oggetto di classe HttpURLConnection per stabilire la connessione
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(urlRichiesta);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "it-IT");
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.close();
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append("\n");
+            }
+            Log.i("richiediDistanza", "dati ricevuti!");
+            Log.i("richiediDistanza", "Dati: " + response);
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+    /**Alla pressione del tasto Google Maps si aprirà l'applicazione Maps per visualizzare il logo dell'incontro direttamente sulla mappa, con la possibilità di ottenere le indicazioni o attivare il navigatore*/
+    public void btnGoogleMapsOnClick(View view) {
+        String luogo = ((EditText) findViewById(R.id.txtLuogo)).getText().toString();
+        Uri uri = Uri.parse("geo:0,0?q=" + luogo);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+        else
+            Toast.makeText(this, "Google Maps non installato su questo dispositivo!", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
-    protected void onDestroy() {
-        //aggiornaEvento();
-        super.onDestroy();
+    public void onLocationChanged(Location location) {
+        String luogo = ((EditText) findViewById(R.id.txtLuogo)).getText().toString();
+        String partenza = location.getLatitude() + "," + location.getLongitude();
+        new RichiediDati().execute(partenza, luogo.replaceAll("\\s+", ""));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    /**Necessario per eseguire operazioni tramite Internet*/
+    private class RichiediDati extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... params) {
+            Log.i("RichiediDati", params[0]);
+            try{
+                String urlParameters = "fName=" + URLEncoder.encode("???", "UTF-8") + "&lName=" + URLEncoder.encode("???", "UTF-8");
+                String url = "http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + params[0] + "&destinations=" + params[1] + "&sensor=false&mode=car";
+                Log.i("RichiediDati", url);
+                final String[] dati = estraiDati(richiediDistanza(url, urlParameters));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((TextView) findViewById(R.id.txtDistanzaTempo)).setText(dati[0] + " (" + dati[1] + ")");
+                    }
+                });
+            }catch(Exception ex){
+
+            }
+            return null;
+        }
     }
 }
